2d6d1c2d2c460776bf2ab9314f8a4bd243c7558c
Beniah <diallobeniah@gmail.com>
2024-04-09 14:32:54 +0000

    "Replace default App component with a new component that rounds grades and displays them"
    This commit removes the default App component that displays a logo and a link to the React website. It adds a new App component that takes an array of grades, rounds them according to the rules specified in the `roundGrades` function, and displays the rounded grades in an unordered list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function roundGrades(grades) {
+    return grades.map(grade => {
+        if (grade < 38 || grade % 5 < 3) {
+            return grade;
+        } else {
+            return grade + (5 - grade % 5);
+        }
+    });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const grades = [25, 37, 83];
+    const roundedGrades = roundGrades(grades);
+
+    return (
+        <div>
+            <h1>Rounded Grades</h1>
+            <ul>
+                {roundedGrades.map((grade, index) => (
+                    <li key={index}>{grade}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
